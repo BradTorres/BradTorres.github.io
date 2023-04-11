a3c1b8422f802e76f5be1fe39cab4a1b04280ed0
BradTorres <widowhsu624forever@outlook.com>
2023-04-11 18:57:02 +0800

    Site updated: 2023-04-11 18:56:59

--- dist/music.js
+++ dist/music.js
@@ -6,13 +6,13 @@ const ap = new APlayer({
 	{
         name: "Welcome To New York (Live)",
         artist: 'Taylor Swift',
-        url: 'http://ws.stream.qqmusic.qq.com/C400001eA8L60LIUmF.m4a?guid=483925709&vkey=0032D37C6AA567F1AE27452E3F7F8725EBE7ACC2A1B0CA1DA0271E8E7647AB483ED6A44BC4C353E72C7AC2D3B1D334DD87F5EC08BEC58961&uin=&fromtag=120032',
+        url: 'http://ws.stream.qqmusic.qq.com/C400001eA8L60LIUmF.m4a?guid=804088999&vkey=BD8506C031F2B112B4D33602C36BB61086718DEE468BA907FD9FC58524F81EEDA804496AB7789D824D9BE8CB0A8273CAD6DA0ECFA3A007BA&uin=&fromtag=120032',
         cover: 'http://p1.music.126.net/W_5XiCv3rGS1-J7EXpHSCQ==/18885211718782327.jpg?param=130y130',
       },
 	  {
         name: 'Shake It Off (Live)',
         artist: 'Taylor Swift',
-        url: 'http://ws.stream.qqmusic.qq.com/C400003KJZyY2sRxEJ.m4a?guid=914452646&vkey=4D3FA7FC5FA64F7CEE7E47DB3E35515DFF3541DED01BC5D717CEAF1D6AE700574215D4FE502C2D7D4844DFB7E46B8B9997A0BABED8003A58&uin=&fromtag=120032',
+        url: 'http://music.163.com/song/media/outer/url?id=1852211573.mp3',
         cover: 'http://p2.music.126.net/Wx5GNJEpay2JbfVUJc4Aew==/109951163094853876.jpg?param=130y130',
       },
       {
